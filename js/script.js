b2ae9f9a77642376e4842b4388b8b76466aacef0
Anthony Liberatore <ajliberatore@gmail.com>
2015-05-28 13:49:25 -0400

    pushVersion1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// FILTERED NOFILTER
+
+// Once user is autenticated...
+
+// Search for images with #nofilter 
+
+/*  From this search, find the photos that have a 
+	tag different from 'normal'.
+*/
+
+// Display all these photos
